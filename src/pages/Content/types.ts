072b084dcc03481415e621338d4e4c6de3b4ab24
kouroshjaberi11 <kourosh.jaberi@mail.utoronto.ca>
2023-11-28 12:45:41 -0500

    Reverted changes to types.ts

--- src/pages/Content/types.ts
+++ src/pages/Content/types.ts
@@ -1,5 +1,4 @@
 import { EIP1193_ERROR_CODES } from './window-provider/eip-1193';
-import { Eip1193Provider } from "ethers";
 
 export type WalletProvider = {
   providerInfo?: {
@@ -100,26 +99,4 @@ export type AAExtensionConfigPayload = {
   shouldReload?: boolean;
   [prop: string]: unknown;
 };
-
-
-interface EIP6963ProviderInfo {
-  uuid: string;
-  name: string;
-  icon: string;
-  rdns: string;
-}
-
-interface EIP6963ProviderDetail {
-  info: EIP6963ProviderInfo;
-  provider: Eip1193Provider;
-}
-
-interface EIP6963AnnounceProviderEvent extends CustomEvent {
-  type: "eip6963:announceProvider";
-  detail: EIP6963ProviderDetail;
-}
-
-interface EIP6963RequestProviderEvent extends Event {
-  type: "eip6963:requestProvider";
-}
 
